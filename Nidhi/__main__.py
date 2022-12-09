e88c04fb8a1d6ecc9c2c8ead0d7029c09a8185f0
Neeraj Kumar <111124378+itsneerja@users.noreply.github.com>
2022-12-09 20:57:33 +0530

    Update __main__.py

--- Nidhi/__main__.py
+++ Nidhi/__main__.py
@@ -15,7 +15,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
  
-bsdk = "» ʙᴏᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ... !"
+ramdi = "» ʙᴏᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ... !"
 
 print(ramdi)
 
