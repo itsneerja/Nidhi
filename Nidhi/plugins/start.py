4104e3f277869b290db6e478d6f9dcde04bea7c3
Neeraj Kumar <111124378+itsneerja@users.noreply.github.com>
2022-12-09 21:06:56 +0530

    Add files via upload

---
+++ Nidhi/plugins/start.py
@@ -0,0 +1,40 @@
+from Nidhi import Nidhi, BOT_USERNAME
+from Config import Config
+from telethon import events, Button
+
+PM_START_TEXT = """
+ʜᴇʏ! {} 🥀
+─────────────────────────
+• **ɪ'ᴍ ᴀ ꜱɪᴍᴘʟᴇ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴜꜱɪᴄ ʙᴏᴛ**.
+"""
+
+@Nidhi.on(events.NewMessage(pattern="^[?!/]start$"))
+async def start(event):
+    if Config.MANAGEMENT_MODE == "ENABLE":
+        return
+    if event.is_private:
+       await event.client.send_file(event.chat_id,
+             Config.START_IMG,
+             caption=PM_START_TEXT.format(event.sender.first_name), 
+             buttons=[
+        [Button.url(" ᴀᴅᴅ ᴍᴇ", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("🥀ꜱᴜᴘᴘᴏʀᴛ", f"https://t.me/{Config.SUPPORT}"), Button.url("🍂 ᴜᴘᴅᴀᴛᴇꜱ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("✨ ᴄᴏᴍᴍᴀɴᴅꜱ", data="help"), Button.url("🎓 ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/itsneerja")]])
+       return
+
+    if event.is_group:
+       await event.reply("**ʜᴇʏ! ɪ'ᴍ ᴀʟɪᴠᴇ 🌹**")
+       return
+
+
+
+@Nidhi.on(events.callbackquery.CallbackQuery(data="start"))
+async def _(event):
+    if Config.MANAGEMENT_MODE == "ENABLE":
+        return
+    if event.is_private:
+       await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
+        [Button.url("ᴀᴅᴅ ᴍᴇ", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("🥀 ꜱᴜᴘᴘᴏʀᴛ", f"https://t.me/{Config.SUPPORT}"), Button.url("🍂 ᴜᴘᴅᴀᴛᴇꜱ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("✨ ᴄᴏᴍᴍᴀɴᴅꜱ", data="help"), Button.url("🎓 ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/itsneerja")]])
+       return
